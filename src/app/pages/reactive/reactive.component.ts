6387630e49cbb610eed47eafbbfb5d18bf0473f6
Wolf08 <alejandro.montecillo@gmail.com>
2021-11-08 17:00:59 -0600

    :sparkles: Validations

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -19,11 +19,13 @@ export class ReactiveComponent implements OnInit {
 
   crearFormulario(){
 
+    //Validadores sincronos
     this.forma = this.fb.group({
-      nombre: [''],
-      apellido: [''],
-      correo: [''],
-    });
+
+      nombre  : ['',[Validators.required,Validators.minLength(5)], ],
+      apellido: ['',[Validators.required,Validators.minLength(5)], ],
+      correo  : ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'),]],
+    })
 
   }
 
