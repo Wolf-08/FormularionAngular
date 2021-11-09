8fe9c757eff1e0592defa6ba5e3552d48fb22b4c
Wolf08 <alejandro.montecillo@gmail.com>
2021-11-08 20:53:24 -0600

    :sparkles: Async validators

--- src/app/pages/reactive/reactive.component.ts
+++ src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidadoresService } from 'src/app/services/validadores.service';
 
 @Component({
   selector: 'app-reactive',
@@ -10,7 +11,8 @@ export class ReactiveComponent implements OnInit {
 
   forma!: FormGroup; 
 
-  constructor( private fb: FormBuilder) { 
+  constructor( private fb: FormBuilder,
+    private validadores: ValidadoresService) { 
   this.crearFormulario();
   this.cargarDataAlFormulario();
   }
@@ -31,8 +33,14 @@ export class ReactiveComponent implements OnInit {
 
   get ciudadNoValido() {
     return this.forma.get('direccion.ciudad')?.invalid && this.forma.get('direccion.ciudad')?.touched
+    
   }
-
+  get usuarioNoValido() {
+    return this.forma.get('usuario')?.invalid && this.forma.get('usuario')?.touched
+  }
+  get pass1NoValido() {
+    return this.forma.get('pass1')?.invalid && this.forma.get('pass1')?.touched;
+  }
   get pass2NoValido() {
     const pass1 = this.forma.get('pass1')?.value;
     const pass2 = this.forma.get('pass2')?.value;
@@ -40,20 +48,23 @@ export class ReactiveComponent implements OnInit {
     return ( pass1 === pass2 ) ? false : true;
   }
 
-  crearFormulario(){
+  crearFormulario() {
 
-    //Validadores sincronos
     this.forma = this.fb.group({
-
-      nombre  : ['',[Validators.required,Validators.minLength(5)], ],
-      apellido: ['',[Validators.required,Validators.minLength(5)], ],
-      correo  : ['', [Validators.required,Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$'),]],
-      direccion : this.fb.group({
-        distrito: ['',Validators.required],
-        ciudad: ['',Validators.required]
+      nombre  : ['', [ Validators.required, Validators.minLength(5) ]  ],
+      apellido: ['', [Validators.required, this.validadores.noHerrera ] ],
+      correo  : ['', [ Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')] ],
+      usuario : ['', , this.validadores.existeUsuario ],
+      pass1   : ['', Validators.required ],
+      pass2   : ['', Validators.required ],
+      direccion: this.fb.group({
+        distrito: ['', Validators.required ],
+        ciudad  : ['', Validators.required ],
       }),
-      pasatiempos: this.fb.array([]),
-    })
+      pasatiempos: this.fb.array([])
+    },{
+      validators: this.validadores.passwordsIguales('pass1','pass2')
+    });
 
   }
   cargarDataAlFormulario() {
@@ -61,8 +72,8 @@ export class ReactiveComponent implements OnInit {
     // this.forma.setValue({
     this.forma.reset({
       nombre: 'Alejandro',
-      apellido: 'Sanchez',
-      correo: 'fernando@gmail.com',
+      apellido: 'Montecillo',
+      correo: 'alejandro@gmail.com',
       pass1: '123',
       pass2: '123',
       direccion: {
